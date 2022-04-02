c06835740dafbf3bcc81b727eebe38374abd5a8a
xilingyuli <xilingyuli@gmail.com>
2022-04-02 18:09:03 +0800

    避免发邮件失败

--- send_message.py
+++ send_message.py
@@ -1,22 +1,26 @@
 from smtplib import SMTP_SSL
 from email.mime.text import MIMEText
 
+import log_message
 
 receive_addr = "786979248@qq.com"
 
 
 def send_message(message):
-    # 填写真实的发邮件服务器用户名、密码
-    user = 'xilingyuli_test@163.com'
-    password = 'DWRSKQUTZNTBFKOH'
-    # 邮件内容
-    msg = MIMEText(message, 'plain', _charset="utf-8")
-    msg["Subject"] = "GJTool"
-    msg["from"] = "xilingyuli_test"
-    msg["to"] = "GJTool"
-    msg["Cc"] = ""
-    with SMTP_SSL(host="smtp.163.com", port=465) as smtp:
-        # 登录发邮件服务器
-        smtp.login(user=user, password=password)
-        # 实际发送、接收邮件配置
-        smtp.sendmail(from_addr=user, to_addrs=receive_addr, msg=msg.as_string())
+    try:
+        # 填写真实的发邮件服务器用户名、密码
+        user = 'xilingyuli_test@163.com'
+        password = 'PECWVHHACWBVLSBA'
+        # 邮件内容
+        msg = MIMEText(message, 'plain', _charset="utf-8")
+        msg["Subject"] = "GJTool"
+        msg["from"] = "xilingyuli_test"
+        msg["to"] = "GJTool"
+        msg["Cc"] = ""
+        with SMTP_SSL(host="smtp.163.com", port=465) as smtp:
+            # 登录发邮件服务器
+            smtp.login(user=user, password=password)
+            # 实际发送、接收邮件配置
+            smtp.sendmail(from_addr=user, to_addrs=receive_addr, msg=msg.as_string())
+    except:
+        log_message.log_error(message)
