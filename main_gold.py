ec46eb2fa0a1498d75d2771f14ecd77fabb5a0a1
xilingyuli <xilingyuli@gmail.com>
2022-05-14 16:14:03 +0800

    修复邮件bug

--- main_gold.py
+++ main_gold.py
@@ -21,7 +21,7 @@ def each_role_action(region_count, role_index):
     if has_gold and cfg.auto_dig_gold_symbols:
         dig_result = dig_huaixiucun.try_dig_map()
         if dig_result:
-            gold_box_images.append(pyautogui.screenshot(cfg.screenshot_region))
+            gold_box_images.append(pyautogui.screenshot(region=cfg.screenshot_region))
         role_action.goto_zhilingjing()
     csv_message.set_gold_symbols(region_count, role_index, has_gold, int(datetime.datetime.now().timestamp()), dig_result, region_count, role_index)
 
--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from gold_symbol import role_action_gold, dig_changheshan, dig_huaixiucun, dig_h
 from message import send_message
 
 time.sleep(3)
-# send_message.send_message('test image', [pyautogui.screenshot(cfg.screenshot_region)])
+# send_message.send_message('test image', [pyautogui.screenshot(region=cfg.screenshot_region)])
 # role_action_gold.try_kill_monster()
 # role_action_gold.move_to_box_mark_in_sky()
 # print(role_loc.get_current_loc())
