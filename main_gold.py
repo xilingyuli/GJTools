5670a6dba489f020a094018fbdec5a18556eab8f
xilingyuli <xilingyuli@gmail.com>
2022-05-18 23:50:23 +0800

    终南山扩区

--- main_gold.py
+++ main_gold.py
@@ -18,7 +18,7 @@ def do_dig_map(region_count, role_index):
     if region_count == 0:
         return dig_huaixiucun.try_dig_map()
     if region_count == 1:
-        return dig_zhongnanshan.try_dig_map()
+        return dig_huaixiucun.try_dig_map()
     if region_count == 2:
         return dig_huahai.try_dig_map()
     return False
